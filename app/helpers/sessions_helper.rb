2fe11b0e5601a98a14862562c143d373a79783bd
goto-ui <onion753@gmail.com>
2020-03-15 07:20:46 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  # 渡されたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
 
   # 記憶トークンcookieに対応するユーザーを返す
   def current_user
@@ -41,6 +46,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
   
 end
